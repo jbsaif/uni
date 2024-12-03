cc2aa7bbe1a2d17badf48c8630c190b3e4a3f66b
Junaeed Bin Saif <junaeed.cse@gmail.com>
2024-12-03 19:40:26 +0600

    Add files via upload

---
+++ Beecrowd/Variables and Data types/Stringcharacter/The Output.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,s;
+    char str[90];
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        scanf("%s",str);
+        printf("gzuz\n",str);
+    }
+
+    return 0;
+}
